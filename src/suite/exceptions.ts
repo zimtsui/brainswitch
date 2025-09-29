977da75ea94d493d54bbcdf0b5dfe9e465c941e8
Zim <zimtsui@gmail.com>
2025-09-29 18:26:57 +0800

    no suite

--- src/suite/exceptions.ts
+++
@@ -1,4 +0,0 @@
-export class LogicError extends Error {}
-export class Thrown extends LogicError {}
-export class Opposition extends LogicError {}
-export class Rejection extends LogicError {}
--- src/suite/exports.ts
+++
@@ -1,9 +0,0 @@
-export * as Evaluate from './nodes/evaluate.summarize.ts';
-export * as Optimize from './nodes/optimize.summarize.ts';
-export * from './nodes/edit/edit.ts';
-export { default as Assets } from '#suite/assets';
-export * from './nodes/progressing.ts';
-export * as Staging from './nodes/staging.ts';
-export * from './suite.ts'
-export * from '#suite/config';
-export * from './exceptions.ts';
