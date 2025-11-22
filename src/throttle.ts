a977ef034703da5294bd69481500a2a4ebdd1ae2
Zim <zimtsui@gmail.com>
2025-11-22 15:37:22 +0800

    throttle

--- src/throttle.ts
+++ src/throttle.ts
@@ -4,6 +4,7 @@ import { Mutex } from '@zimtsui/coroutine-locks';
 
 export class Throttle {
     private valve = new Mutex();
+    private timer?: NodeJS.Timeout;
     private interval: number;
     public constructor(private rpm: number) {
         this.interval = Math.ceil(60*1000 / this.rpm);
@@ -11,8 +12,24 @@ export class Throttle {
 
     public async requests(ctx: InferenceContext): Promise<void> {
         await ctx.busy?.acquireRead();
-        await this.valve.acquire();
-        setTimeout(() => void this.valve.release(), this.interval);
-        ctx.busy?.releaseRead();
+        const pwr = Promise.withResolvers<void>();
+        const callback = () => pwr.reject(ctx.signal?.reason);
+        ctx.signal?.addEventListener('abort', callback);
+        const waiting = this.valve.acquire();
+        try {
+            await Promise.race([waiting, pwr.promise]);
+            this.timer = setTimeout(() => void this.valve.release(), this.interval);
+        } catch (e) {
+            waiting.then(() => this.valve.release()).catch(() => {});
+            throw e;
+        } finally {
+            ctx.signal?.removeEventListener('abort', callback);
+            ctx.busy?.releaseRead();
+        }
+    }
+
+    public throw(e: Error) {
+        if (this.timer) clearTimeout(this.timer);
+        this.valve.throw(e);
     }
 }
