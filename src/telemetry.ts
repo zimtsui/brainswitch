595719f731369201f75f56cb6ac0a5fe83a9e563
Zim <zimtsui@gmail.com>
2026-03-10 18:07:21 +0800

    up

--- src/telemetry.ts
+++ src/telemetry.ts
@@ -1,12 +1,12 @@
 import { Channel } from '@zimtsui/typelog';
 import * as Presets from '@zimtsui/typelog/presets';
-import { exporter } from '@zimtsui/typelog/exporter';
+import { Exporter } from '@zimtsui/typelog/exporter';
 
 
 export const logger = {
     inference: Channel.create<typeof Presets.Level, string>(
         Presets.Level,
-        (chunk: string, level) => exporter.stream({
+        (chunk: string, level) => Exporter.getGlobalExporter().stream({
             scope: '@zimtsui/brainswitch',
             channel: 'Inference',
             level,
@@ -15,7 +15,7 @@ export const logger = {
     ),
     message: Channel.create<typeof Presets.Level, unknown>(
         Presets.Level,
-        (payload: unknown, level) => exporter.monolith({
+        (payload: unknown, level) => Exporter.getGlobalExporter().monolith({
             scope: '@zimtsui/brainswitch',
             channel: 'Message',
             level,
