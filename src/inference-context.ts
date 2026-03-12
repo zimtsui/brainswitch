423f505465cc9cd50fd85263ab63e03d1288b63b
Zim <zimtsui@gmail.com>
2026-03-12 19:51:42 +0800

    cost

--- src/inference-context.ts
+++ src/inference-context.ts
@@ -4,5 +4,5 @@ import { RWLock } from '@zimtsui/coroutine-locks';
 export interface InferenceContext {
     busy: RWLock | null;
     signal: AbortSignal | null;
-    cost(deltaCost: number): void;
+    cost?(deltaCost: number): void;
 }
