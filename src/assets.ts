7c62fa6502e0a4cb8dd53835d2a65aa8461dc574
Zim <zimtsui@gmail.com>
2026-03-27 00:38:35 +0800

    dev

--- src/assets.ts
+++ src/assets.ts
@@ -3,10 +3,9 @@ import { RoleMessage } from '@zimtsui/brainswitch';
 
 
 export default {
-	verbatim: {
+    verbatim: {
         instruction: RoleMessage.Part.Text.paragraph(loadtext(import.meta.resolve('../assets/verbatim/instruction.md'))),
         declarations: RoleMessage.Part.Text.paragraph(loadtext(import.meta.resolve('../assets/verbatim/declarations.handlebars'))),
         requests: RoleMessage.Part.Text.paragraph(loadtext(import.meta.resolve('../assets/verbatim/requests.handlebars'))),
-        response: RoleMessage.Part.Text.paragraph(loadtext(import.meta.resolve('../assets/verbatim/response.handlebars'))),
-	},
+    },
 } as const;
