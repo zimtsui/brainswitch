87b62826a1cbd8ddcf567846442fc0ebd6def9d3
Zim <zimtsui@gmail.com>
2026-03-28 03:24:52 +0800

    fix

--- src/assets.ts
+++ src/assets.ts
@@ -5,6 +5,5 @@ export default {
     verbatim: {
         instruction: loadtext(import.meta.resolve('../assets/verbatim/instruction.md')),
         declarations: loadtext(import.meta.resolve('../assets/verbatim/declarations.handlebars')),
-        request: loadtext(import.meta.resolve('../assets/verbatim/request.handlebars')),
     },
 } as const;
